d51fbf1f3070585025403295bb4bc7856ad43a0e
tepesalexandru <tepes_alexandru@outlook.com>
2021-03-17 22:31:58 +0200

    Add project files.

---
+++ OOP_Facultate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP_Facultate
+{
+    class Stars
+    {
+        int rows;
+        public Stars(int rows)
+        {
+            this.rows = rows;
+        }
+
+        public void DrawStars()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        ~Stars()
+        {
+            Console.WriteLine("Instanta clasei Stars a fost distrusa.");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stars s = new Stars(5);
+            s.DrawStars();
+            Console.ReadKey();
+        }
+    }
+}
